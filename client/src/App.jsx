4dfbd2460cfd549089193ea53740a77a0fbabcb0
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-02-17 22:46:04 -0500

    update app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,11 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import TaskRoutes from './routes/TaskRoutes';
 
-function App() {
+export const App = () => {
 	return (
-		<div class={styles.App}>
-			<header class={styles.header}>
-				<img src={logo} class={styles.logo} alt="logo" />
-				<p>
-					Edit <code>src/App.jsx</code> and save to reload.
-				</p>
-				<a class={styles.link} href="https://github.com/solidjs/solid" target="_blank" rel="noopener noreferrer">
-					Learn Solid
-				</a>
-			</header>
-		</div>
+		<>
+			<TaskRoutes />
+		</>
 	);
-}
+};
 
 export default App;
