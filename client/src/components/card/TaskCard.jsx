0f60279e98ae2e9ebc4e05a2e8848dc342b5e100
andresNeveu <victor.sapuyes@correounivalle.edu.co>
2023-02-27 00:03:49 -0500

    add handle delete and mark buttons

--- client/src/components/card/TaskCard.jsx
+++ client/src/components/card/TaskCard.jsx
@@ -1,23 +1,46 @@
+import { createSignal } from 'solid-js';
+import { useAuth } from '../../hooks/useAuth';
+import { deleteTask, markTask, unMarkTask } from '../../services/tasks.service';
 import DeleteButton from '../button/DeleteButton';
 import MarkButton from '../button/MarkButton';
 import style from '../styles/Card.module.css';
 
 const TaskCard = (props) => {
-	const handleClick = (e) => {
-		console.log('here');
+	const { token } = useAuth();
+	const [mark, setMark] = createSignal(props.task.is_done);
+
+	const handleDeleteButton = async (e) => {
+		const res = await deleteTask(token(), props.task.id);
+		if (res.status === 200) {
+			props.delete(props.id);
+		}
 	};
+	const handleMarkButton = async (e) => {
+		if (mark()) {
+			const res = await unMarkTask(token(), props.task.id);
+			if (res.status === 200) {
+				setMark(false);
+			}
+		} else {
+			const res = await markTask(token(), props.task.id);
+			if (res.status === 200) {
+				setMark(true);
+			}
+		}
+	};
+
 	return (
 		<div class={style.cardContainer}>
 			<div>
 				<div>{`Id: ${props.task.id}`}</div>
 				<div>{`Title: ${props.task.title}`}</div>
-				<div>{`Done: ${props.task.is_done ? 'yes' : 'no'}`}</div>
+				<div>{`Done: ${mark() ? 'yes' : 'no'}`}</div>
 				<div>{`Created At: ${props.task.created_at.slice(0, 10)}`}</div>
 			</div>
 			<div>
-				<MarkButton onClick={handleClick} />
+				<MarkButton onClick={handleMarkButton} mark={mark()} />
 				<br />
-				<DeleteButton onClick={handleClick} />
+				<DeleteButton onClick={handleDeleteButton} />
 			</div>
 		</div>
 	);
